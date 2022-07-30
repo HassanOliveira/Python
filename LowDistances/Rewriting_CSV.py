488eafa1a135e802d7083be295826b3e6a9f9157
HassanOliveira <hassan_bittencourt@hotmail.com>
2022-07-29 23:44:40 -0300

    LowDistance

---
+++ LowDistances/Rewriting_CSV.py
@@ -0,0 +1,9 @@
+from codecs import utf_16_be_decode
+import csv
+
+with open("all_cities.csv", "r", encoding="utf-8") as source:
+    reader = csv.reader(source, delimiter=';')
+    with open("all_cities_filter.csv", "w", encoding="utf-8") as result:
+        writer = csv.writer(result, delimiter=';')
+        for r in reader:
+            writer.writerow((r[0], r[2], r[7], r[19]))
